bb400fe65f5ea5910158108604692aed27d60d90
Kevin <kandres@elevensystems.com>
2020-02-20 18:17:52 -0600

    remove important information

--- src/app/utils/const.ts
+++ src/app/utils/const.ts
@@ -1,12 +1,12 @@
 export const config = {
-    apiKey: "AIzaSyAzt-Jh02DKYg0yxg_im3Vndu6jw0-HIP0",
-    authDomain: "fir-angular-42851.firebaseapp.com",
-    databaseURL: "https://fir-angular-42851.firebaseio.com",
-    projectId: "fir-angular-42851",
-    storageBucket: "fir-angular-42851.appspot.com",
-    messagingSenderId: "974348078274",
-    appId: "1:974348078274:web:0b3e50b66a282cdb8c7f5b",
-    measurementId: "G-4HGQ6TS929"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 // 5DnBHnoUyBaruRnGM3ofedDcc4l2
